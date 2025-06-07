45b212beb7d691affa051dd21912321c13c4d15a
fffbandeira <fffbandeira@hotmail.com>
2025-06-07 21:23:37 +0000

    resolução exercício

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,18 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+fluxo_caracteres = "string"
 
 # Declare variáveis de cada tipo numérico
-
+numero = 1976
+flutuantes = 7.6
 
 # Declare uma variável do tipo booleano
+x = True
+y = False
+
+print(type(fluxo_caracteres))
+print(type(x))
+print(type(numero))
+print(type(flutuantes))
+print(type(y))
